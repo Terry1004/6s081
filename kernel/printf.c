6947765bade2921a76925901fb7404b2821cb596
Huang Yiming <hym961004@gmail.com>
2025-03-21 14:35:03 +0800

    implement backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -117,6 +117,8 @@ printf(char *fmt, ...)
 void
 panic(char *s)
 {
+  backtrace();
+
   pr.locking = 0;
   printf("panic: ");
   printf(s);
@@ -131,4 +133,20 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace(void)
+{
+  uint64 fp = r_fp();
+  uint64 prev_fp = *(uint64*)(fp - 16);
+
+  printf("backtrace:\n");
+  uint64 sstart = PGROUNDUP(fp);
+  uint64 send = PGROUNDDOWN(fp);
+  while (prev_fp <= sstart && prev_fp > send) {
+    printf("%p\n", *(uint64*)(fp - 8));
+    fp = prev_fp;
+    prev_fp = *(uint64*)(fp - 16);
+  }
 }
